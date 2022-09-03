a16245783e27c8c821dbb31093635a7947aec139
vibhatsu08 <mistryvedant17@gmail.com>
2022-09-04 00:59:21 +0530

    Added a window.onload function to the existing code, so now whenever the browser window is loaded the sidepanel's width is by default set to zero, unless the user clicks on the hamburger menu and wants to open it by choice.

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ openPanel = () => {
 closePanel = () => {
     document.getElementById('mysidepanel').style.width = '0px';
 }
+// Using this function, because i am facing a bug, where the sidepanel automatically opens on device browsers with width less than 510px.
+window.onload = () => {
+    document.getElementById('mysidepanel').style.width = '0px';
+}
